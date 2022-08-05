a05272118894e05b8a824da5f2d1af112283d1ab
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-05 23:43:31 +0900

    문자열 / 8번 / 유효한 팰린드롬

--- src/유효한_팰린드롬_8.java
+++ src/유효한_팰린드롬_8.java
@@ -1,6 +1,22 @@
-public class 유효한_팰린드롬_8 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class 유효한_팰린드롬_8 {
+    public static String solution(String str) {
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        String reverseStr = new StringBuilder(str).reverse().toString();
+        if (str.equals(reverseStr)) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(solution(str));
 
     }
 

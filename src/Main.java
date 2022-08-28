6f783caddcdc8c78d1ff45d8c79e2d2e7a50917f
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-29 00:51:03 +0900

    [INF] / 문장 속 단어 / 문자열

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+// 문자열 - 3번. 문장 속 단어
+public class Main {
+    public static String solution(String str) {
+        String[] strBits = str.split(" ");
+        int max = Integer.MIN_VALUE;
+        String ans = "";
+        for (String x : strBits) {
+            if (x.length() > max) {
+                max = x.length();
+                ans=x;
+            }
+        }
+        return ans;
+    }
 
-// 강의 정답.
-import java.lang.reflect.Array;
-import java.util.*;
-class Main {
-
-    public static void main(String[] args){
-        String str = "helloWorld";
-        char[] charArr = str.toCharArray();
-        System.out.println(Arrays.toString(charArr));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(solution(str));
 
     }
+
 }

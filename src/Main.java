372b756ec46550e68f9fc50554d55e4b6c6e376e
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-09-04 20:03:44 +0900

    [INF] / 큰 수 출력하기 / 1,2차원 배열

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-// 문자열 - 3번. 문장 속 단어
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+// 배열 - 1번. 큰 수 출력하기.
 public class Main {
-    public static String solution(String str) {
-        String[] strBits = str.split(" ");
-        int max = Integer.MIN_VALUE;
-        String ans = "";
-        for (String x : strBits) {
-            if (x.length() > max) {
-                max = x.length();
-                ans=x;
-            }
+    public static int n;
+    public static StringTokenizer st;
+    public static void solution() {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        return ans;
+        System.out.print(arr[0] + " ");
+        for (int i = 1; i < n; i++) {
+            if(arr[i] > arr[i-1]) System.out.print(arr[i] + " ");
+        }
+
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        System.out.println(solution(str));
-
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        solution();
     }
 
 }

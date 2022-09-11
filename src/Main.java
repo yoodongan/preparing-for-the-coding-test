1de9f4b0cf0b03efffe9c26d334495f93757b3e7
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-09-11 13:42:01 +0900

    [INF] / 피보나치 수열 / 1,2차원 배열

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-// 배열 - 3번. 가위 바위 보
+// 배열 - 4번. 피보나치 수열
 public class Main {
-    public static String solution(int[] arr1, int[] arr2) {
-        String answer="";
-        for (int i = 0; i < arr1.length; i++) {
-            if(arr1[i] == arr2[i]) answer+="D";
-            else if(arr1[i]==1 && arr2[i]==3) answer+="A";
-            else if(arr1[i]==2 && arr2[i]==1) answer+="A";
-            else if(arr1[i]==3 && arr2[i]==2) answer+="A";
-            else answer+="B";
+    public static void solution(int n) {
+        int[] arr = new int[n];
+        arr[0] = arr[1] = 1;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
         }
-        return answer;
+        for (int x : arr) {
+            System.out.print(x+ " ");
+        }
+
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer stA = new StringTokenizer(br.readLine());
-        StringTokenizer stB = new StringTokenizer(br.readLine());
-        int[] arrA = new int[n];
-        int[] arrB = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arrA[i] = Integer.parseInt(stA.nextToken());
-            arrB[i] = Integer.parseInt(stB.nextToken());
-        }
-        for(char x : solution(arrA, arrB).toCharArray()) System.out.println(x);
+        solution(n);
 
     }
 }

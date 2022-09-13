92a258469cd707463470b216970d14f2349b3db0
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-09-13 09:15:40 +0900

    [INF] / 뒤집은 소수 / 6

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-// 배열 - 5번. 에라토스테네스 체
+// 배열 - 6번. 뒤집은 소수
 public class Main {
-    public static int solution(int n) {
-        boolean[] arr = new boolean[n+1];
-        for (int i = 0; i < n + 1; i++) {
-            arr[i] = true;
+    public static void solution(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            String number = String.valueOf(arr[i]);
+            StringBuffer sb = new StringBuffer(number);
+            arr[i] = Integer.parseInt(sb.reverse().toString());
         }
-        arr[0] = arr[1] = false;
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            for (int j = i * i; j <= n; j += i) {
-                arr[j] = false;
+        for (int i : arr) {
+            if(isPrime(i)) {
+                System.out.print(i + " ");
             }
         }
-        int cnt =0;
-        for (int i = 0; i <= n; i++) {
-            if(arr[i]) cnt++;
+    }
+    public static boolean isPrime(int n){
+        if(n==1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if(n%i == 0) return false;
         }
-        return cnt;
+        return true;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println(solution(n));
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        solution(arr);
 
     }
 }

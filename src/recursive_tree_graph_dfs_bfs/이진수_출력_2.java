07ef43b78c355f0788184d167da5c8e3ce9689ff
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-11-12 20:04:38 +0900

    [INF] / 이진수_출력_2 / recursive,tree,graph(dfs,bfs)

---
+++ src/recursive_tree_graph_dfs_bfs/이진수_출력_2.java
@@ -0,0 +1,22 @@
+package recursive_tree_graph_dfs_bfs;
+
+import java.util.Scanner;
+
+public class 이진수_출력_2 {
+
+    public static void recursive(int n) {
+        if(n==0) return;
+        else {
+            recursive(n/2);
+            System.out.print(n%2 + " ");
+        }
+    }
+    public static void solution(int n) {
+        recursive(n);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        solution(n);
+    }
+}

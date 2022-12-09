9cbc2e7300ef867ce5180c4eb8a4e37a48566ab3
ryudongan <dbehddks519@naver.com>
2022-12-09 20:32:26 +0900

    [PRG] / 명예의 전당 / Lv1 / 정렬 - 코드 수정, 다시 풀었음.

--- src/PRG/exercise/명예의_전당.java
+++ src/PRG/exercise/명예의_전당.java
@@ -1,28 +1,29 @@
 package PRG.exercise;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class 명예의_전당 {
-    public int[] solution(int k, int[] score) {
+    public static int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> arr = new ArrayList();
-        for (int i=0; i<score.length; i++) {
-            if (i<k-1){
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            if(i <= k-1) {
                 arr.add(score[i]);
                 arr.sort(Collections.reverseOrder());
-                answer[i]=arr.get(arr.size()-1);
-            } else if (i>=k-1){
+                answer[i] = arr.get(arr.size() - 1);  // 내림차순이므로 끝에 있는 것이 최소값.
+            } else {
                 arr.add(score[i]);
                 arr.sort(Collections.reverseOrder());
-                answer[i]=arr.get(k-1);
+                answer[i] = arr.get(k - 1);
             }
         }
         return answer;
     }
-
     public static void main(String[] args) {
         int[] arr = {10, 100, 20, 150, 1, 100, 200};
+        System.out.println(Arrays.toString(solution(3, arr)));
     }
 }

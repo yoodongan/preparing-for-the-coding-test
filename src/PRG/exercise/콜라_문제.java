e1a0883e3adda7bdc3013f1dd6422d1a3ab2913c
ryudongan <dbehddks519@naver.com>
2022-12-16 11:50:07 +0900

    [PRG] / 콜라 문제 / Lv1 / - 수정

--- src/PRG/exercise/콜라_문제.java
+++ src/PRG/exercise/콜라_문제.java
@@ -3,17 +3,15 @@ package PRG.exercise;
 public class 콜라_문제 {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        int tmp = 0;
-        while(true) {
-            if(n % a != 0) tmp+=n%a;
-            n = n/a;
-            answer += (n * b);
-            if(n == 0) break;
-        }
 
-        if (tmp > 0) {
-            answer += (tmp/a) * b;
+        while(true) {
+            if(n<a) break;
+
+            answer += (n/a * b);
+            int tmp = n%a;
+            n = (n/a) * b + tmp;
         }
         return answer;
+
     }
 }

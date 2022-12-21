8585a0662d717d13963e4e4cddf87f9320fc5077
ryudongan <dbehddks519@naver.com>
2022-12-21 15:33:58 +0900

    [PRG] / 음양 더하기, 내적 / Lv1 /

---
+++ src/PRG/exercise/음양_더하기.java
@@ -0,0 +1,12 @@
+package PRG.exercise;
+
+public class 음양_더하기 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0; i< signs.length; i++){
+            if(signs[i]) answer += absolutes[i];
+            else answer -= absolutes[i];
+        }
+        return answer;
+    }
+}

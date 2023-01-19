4786cdb4c043cae1a88b155df83697fd47b07d8a
ryudongan <90627763+yoodongan@users.noreply.github.com>
2023-01-19 21:08:53 +0900

    [PRG] / 예산 / 정렬

---
+++ src/PRG/exercise/예산.java
@@ -0,0 +1,18 @@
+package PRG.exercise;
+
+import java.util.Arrays;
+
+public class 예산 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for (int i = 0; i < d.length; i++) {
+            if (d[i] <= budget) {
+                budget -= d[i];
+                answer++;
+            } else break;
+        }
+
+        return answer;
+    }
+}

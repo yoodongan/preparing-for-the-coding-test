42d0f633bf278c7e407b83af320f6d56ccb5aefb
yoodongan <dbehddks519@naver.com>
2023-01-26 18:27:17 +0900

    [PRG] / 숫자 문자열과 영단어 / 문자열 - 수정

--- src/PRG/exercise/숫자_문자열과_영단어.java
+++ src/PRG/exercise/숫자_문자열과_영단어.java
@@ -4,29 +4,11 @@ package PRG.exercise;
 public class 숫자_문자열과_영단어 {
     public static int solution(String s) {
         String[] str = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-
-        char[] sToChar = s.toCharArray();
-        StringBuilder answer = new StringBuilder();
-        StringBuilder tmp = new StringBuilder();
-        for (int i = 0; i < sToChar.length; i++) {
-            if((sToChar[i] >= 48 && sToChar[i] <=57)) {
-                if (tmp.length() > 0) {
-                    for (int j = 0; j < str.length; j++) {
-                        if (str[j].equals(tmp.toString())) {
-                            int index = j;
-                            answer.append(index);
-                        }
-                    }
-                    tmp.setLength(0);
-                }
-                answer.append(sToChar[i]);
-
-
-            } else {
-                tmp.append(sToChar[i]);
-            }
+        String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        for (int i = 0; i < num.length; i++) {
+            s = s.replace(str[i], num[i]);
         }
-        return Integer.parseInt(String.valueOf(answer));
+        return Integer.parseInt(s);
     }
 
     public static void main(String[] args) {

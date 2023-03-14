5e6e7ad0fce47f3f1ffb2bdfe4448fafabd22c3f
yoodongan <dbehddks519@naver.com>
2023-03-14 19:15:28 +0900

    [PRG] / 문자열 나누기 / 문자열 / lv1 - 복습, 조금 다른 방법으로 풀었음.

--- src/PRG/exercise/문자열_나누기.java
+++ src/PRG/exercise/문자열_나누기.java
@@ -3,22 +3,19 @@ package PRG.exercise;
 public class 문자열_나누기 {
     public static int solution(String s) {
         int answer = 0;
-        char first = s.charAt(0);
-        int firstNum = 0;
-        int secondNum = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (first == s.charAt(i)) {
-                firstNum++;
-            } else {
-                secondNum++;
-            }
-            if (firstNum == secondNum) {
+        char first = ' ';
+        int num1 = 0;
+        int num2 = 0;
+        for(char c : s.toCharArray()) {
+            if(first == ' ') first = c;
+            if(first == c) num1++;
+            else num2++;
+            if(num1 == num2) {
                 answer++;
-                first = s.charAt(i);
+                first = ' ';
             }
-
         }
+        if(first != ' ') answer++;
         return answer;
     }
 

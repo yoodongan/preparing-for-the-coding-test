29457f189aaf3a106ca4c12429d3b6c8275575cc
yoodongan <dbehddks519@naver.com>
2023-03-15 13:20:42 +0900

    [INF] / 문자열 복습

--- src/문자열/String_3.java
+++ src/문자열/String_3.java
@@ -6,21 +6,19 @@ import java.io.InputStreamReader;
 
 public class String_3 {
     public static String solution(String str) {
-        String[] strSplit = str.split(" ");
-        int max_length = 0;
-        String ans = "";
-        for (int i = 0; i < strSplit.length; i++) {
-            if(strSplit[i].length() > max_length) max_length = strSplit[i].length();
-        }
-        for (String x : strSplit) {
-            if(x.length() ==max_length) {
-                ans += x;
+        int max = Integer.MIN_VALUE, pos;
+        String answer = "";
+        while((pos = str.indexOf(" ")) != -1) {
+            String tmp = str.substring(0, pos);
+            if(tmp.length() > max) {
+                max = tmp.length();
+                answer = tmp;
             }
+            str = str.substring(pos+1);
         }
-        return ans;
+        if(str.length() > max) answer = str;
+        return answer;
     }
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();

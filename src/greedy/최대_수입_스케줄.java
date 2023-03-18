9ecfe234f80f8ae15f269fbb7d7dda3e861c73f8
ryudongan <90627763+yoodongan@users.noreply.github.com>
2023-03-18 20:08:53 +0900

    [INF] / 최대 수입 스케줄 / 그리디 - 복습

--- src/greedy/최대_수입_스케줄.java
+++ src/greedy/최대_수입_스케줄.java
@@ -6,32 +6,35 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class 최대_수입_스케줄 {
-    static class Lecture implements Comparable<Lecture> {
-        int price;
-        int time;
-
-        public Lecture(int price, int time) {
-            this.price = price;
-            this.time = time;
+    static class Lecture implements Comparable<Lecture>{
+        int m;  // 금액
+        int d;  // D 일 안에 와서 강연해야 함.
+        Lecture(int m, int d) {
+            this.m = m;
+            this.d = d;
         }
         @Override
         public int compareTo(Lecture l) {
-            return l.time - this.time;
+            return l.d - this.d;
         }
     }
-    public static int solution(int n, ArrayList<Lecture> arr) {
-        int answer = 0;
-        Collections.sort(arr);
-        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
-        int max = arr.get(0).time;
-        int j = 0;
-        for (int i = max; i >= 1; i--) {
-            for (; j < n; j++) {
-                if(arr.get(j).time < i) break;
-                pq.add(arr.get(j).price);
-            }
-            if(!pq.isEmpty()) answer+=pq.poll();
 
+    public static int solution(int n, ArrayList<Lecture> arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Collections.sort(arr);
+        int answer = 0;
+        int day = arr.get(0).d;
+        for(int i=day; i>=1; i--) {
+            for(Lecture lec : arr) {
+                if(lec.d < i) break;
+                if(lec.d == i) {
+                    pq.add(lec.m);
+                }
+            }
+            if(!pq.isEmpty()) {
+                int price = pq.poll();
+                answer += price;
+            }
         }
         return answer;
     }

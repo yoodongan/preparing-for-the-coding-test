1e43259fd4e93522dbad8ebde67c2a34fe3405e9
yoodongan <dbehddks519@naver.com>
2023-03-21 12:59:21 +0900

    [PRG] / 삼총사 / dfs - 조합

--- src/PRG/exercise/삼총사.java
+++ src/PRG/exercise/삼총사.java
@@ -1,46 +1,37 @@
 package PRG.exercise;
 
 public class 삼총사 {
-    public static int answer = 0;
-    public static int[] pm = new int[100];
-    public static int[] arr;
-    public static int index = 0;
-    public static int[] check;
-    public static void dfs(int L, int sum) {
-        if(L==3) {
-            if (sum == 0) {
-                for (int i : pm) {
-                    System.out.print(i + " ");
-                }
-                System.out.println();
-                answer++;
-                return;
-            }
-        }
-        else {
-            for (int i = 0; i < arr.length; i++) {
-                if (check[i] != 1) {
-                    check[i] = 1;
-                    pm[index++] = arr[i];
-                    dfs(L+1, sum+arr[i]);
-                    check[i] = 0;
-                }
-            }
-        }
+    static int answer = 0;
+    static int[] numbers;
+    static boolean[] visited;
 
+
+    public int solution(int[] numbers) {
+        boolean[] visited = new boolean[numbers.length];
+        this.numbers = numbers;
+        this.visited = new boolean[numbers.length];
+        dfs(0, 0, 0);
+        return answer;
     }
 
-    public int solution(int[] number) {
-        check = new int[number.length];
-        arr = number;
-        dfs(0, 0);
-        return answer;
+    private void dfs(int start, int L, int sum) {
+        if(L == 3) {
+            if(sum == 0) answer++;
+            return;
+        }
+
+        for(int i=start; i<numbers.length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                dfs(i+1, L+1, sum+numbers[i]);
+                visited[i] = false;
+            }
+        }
     }
 
     public static void main(String[] args) {
         삼총사 ans = new 삼총사();
         int[] number = {-2, 3, 0, 2, -5};
         System.out.println(ans.solution(number));
-
     }
 }

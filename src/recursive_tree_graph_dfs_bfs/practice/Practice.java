6e496a0dd181a227b285e50701eb5bd4885af91d
yoodongan <dbehddks519@naver.com>
2023-03-24 19:58:06 +0900

    [PRG] / 모의고사 / 완전탐색 / lv1

--- src/recursive_tree_graph_dfs_bfs/practice/Practice.java
+++ src/recursive_tree_graph_dfs_bfs/practice/Practice.java
@@ -1,19 +1,13 @@
 package recursive_tree_graph_dfs_bfs.practice;
 
-public class Practice {
-    static int answer;  // 조합의 답.
-    static int[][] arr = new int[34][34];  // memoization 을 위한 배열. ~33까지 써야 하므로.
-    public static int dfs(int n, int r) {
-        if(arr[n][r] > 0) return arr[n][r];
-        if(n == r || r == 1) return 1;
-        else {
-            return arr[n][r] = dfs(n-1, r-1) + dfs(n-1, r);
-        }
-    }
+import java.util.*;
 
+public class Practice {
     public static void main(String[] args) {
-        int n = 5;
-        int r = 3;
-        System.out.println(dfs(n, r));
+        StringBuilder sb = new StringBuilder();
+        sb.append("34");
+        String s = sb.reverse().toString();
+        System.out.println(s);
+
     }
 }

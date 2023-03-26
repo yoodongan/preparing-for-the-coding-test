8a7b63e8cd812222f3a132b58795fea09b42a157
yoodongan <dbehddks519@naver.com>
2023-03-26 17:49:48 +0900

    [PRG] / 복습 - 기사단원의 무기(lv1)

--- src/PRG/exercise/기사단원의_무기.java
+++ src/PRG/exercise/기사단원의_무기.java
@@ -2,28 +2,22 @@ package PRG.exercise;
 
 //약수 개수 구하기 문제.
 public class 기사단원의_무기 {
-    public static int solution(int number, int limit, int power) {
+    public int count(int number) {
+        int cnt = 0;
+        for(int i=1; i<=number/2; i++) {
+            if(number % i ==0) cnt++;
+        }
+        return cnt+1;
+    }
+    public int solution(int number, int limit, int power) {
         int answer = 0;
-        for (int i = 1; i <= number; i++) {
-            if (getNumCount(i) <= limit) {
-                answer += getNumCount(i);
-             } else {
-                answer += power;
-            }
+        int[] ans = new int[number+1];
+
+        for(int i=1; i<=number; i++) {
+            ans[i] = count(i);
+            if(ans[i] > limit) answer+=power;
+            else answer+=ans[i];
         }
         return answer;
-    }
-    public static int getNumCount(int n) {
-        int count = 0;
-        for (int i = 1; i * i <= n; i++) {
-            if(i*i == n) count++;
-            else if(n%i == 0) count+=2;
-        }
-        return count;
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println(solution(5, 3, 2));
     }
 }

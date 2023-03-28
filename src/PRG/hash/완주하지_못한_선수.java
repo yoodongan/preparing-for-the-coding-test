ed8a92a66cb692f1208d0dbe8a0599195a55d932
yoodongan <dbehddks519@naver.com>
2023-03-28 11:51:01 +0900

    [PRG] / 완주하지 못한 선수 / 해시 / lv1 - 복습

--- src/PRG/hash/완주하지_못한_선수.java
+++ src/PRG/hash/완주하지_못한_선수.java
@@ -6,13 +6,17 @@ import java.util.Map;
 
 public class 완주하지_못한_선수 {
     public String solution(String[] participant, String[] completion) {
-        String answer ="";
-        Map<String, Integer> players = new HashMap<>();
-        for(String player : participant) players.put(player, players.getOrDefault(player, 0) + 1);
-        for(String completed : completion) players.put(completed, players.get(completed)-1);
-        for (String key : players.keySet()) {
-            if(players.get(key) != 0) {  // 0 이 아니면 완주하지 않은 사람.
-                answer = key;
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+        for (String s : map.keySet()) {
+            if (map.get(s) != 0) {
+                answer = s;
             }
         }
         return answer;

646d313ba45ea9cc65312847b538ae1818a9ae33
ryudongan <90627763+yoodongan@users.noreply.github.com>
2023-04-23 21:09:57 +0900

    [PRG] / 귤 고르기 / 그리디, 해시 / lv2 - 다시 풀어봄. 그리디 와 해시를 활용해서 푸는 문제이다.

--- src/PRG/lv2/귤_고르기.java
+++ src/PRG/lv2/귤_고르기.java
@@ -3,38 +3,33 @@ package PRG.lv2;
 import java.util.*;
 
 public class 귤_고르기 {
-    public static int solution(int k, int[] tangerine) {
+    public int solution(int k, int[] tangerine) {
         int answer = 0;
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < tangerine.length; i++) {
-            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(Integer i : tangerine) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
         List<Integer> arr = new ArrayList<>(map.values());
         Collections.sort(arr);
+
         int findNum = tangerine.length - k;
+
         int sum = 0;
-        int index = 0;
-        for (Integer integer : arr) {
-            sum += integer;
+        int idx = 0;
+        for(Integer in : arr ) {
+            sum += in;
             if(sum > findNum) {
-                answer = arr.size() - index;
+                answer = arr.size() - idx;
                 break;
             }
             else if(sum == findNum) {
-                answer = arr.size() - (index+1);
+                answer = arr.size() - (idx+1);
                 break;
             }
-            index++;
+            idx++;
         }
+
         return answer;
-    }
-
-
-    public static void main(String[] args) {
-        int k = 4;
-        int[] arr = {1, 3, 2, 5, 4, 5, 2, 3};
-        System.out.println(solution(k, arr));
-
     }
 }
 

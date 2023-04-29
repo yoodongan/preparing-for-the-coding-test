ea5fe410525cf777c74fe1a017d2f7a30ccf245c
yoodongan <dbehddks519@naver.com>
2023-04-29 16:43:10 +0900

    [PRG] / 할인 행사 / 해시 / lv2

--- src/PRG/lv2/할인_행사.java
+++ src/PRG/lv2/할인_행사.java
@@ -4,29 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class 할인_행사 {
+    final int STREIGHT = 10;
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int p1 = 0;
-        int p2 = 10;
-        boolean flag = true;
-        while(p1 + 10 <= discount.length) {
-            Map<String, Integer> map = new HashMap<>();
-            for (int i = 0; i < want.length; i++) {
-                map.put(want[i], number[i]);
-            }
-            for (int i = p1; i < p2; i++) {
-                if(map.get(discount[i]) == null) break;
-                map.put(discount[i], map.get(discount[i]) -1);
-                if(map.get(discount[i]) < 0) {
-                    break;
-                }
-            }
-            for (Integer value : map.values()) {
-                if(value < 0) flag = false;
-            }
-            if(!flag) answer++;
-            p1++; p2++;
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<want.length; i++) {
+            map.put(want[i], number[i]);
         }
+        Map<String, Integer> discountMap = new HashMap<>();
+        for(int i=0; i<STREIGHT; i++) {
+            discountMap.put(discount[i], discountMap.getOrDefault(discount[i], 0) + 1);
+        }
+        if(canJoin(map, discountMap)) answer++;
+
+        for(int i=1; i <= discount.length - STREIGHT; i++) {
+            discountMap.put(discount[i-1], discountMap.get(discount[i-1]) - 1);
+            discountMap.put(discount[i+STREIGHT-1], discountMap.getOrDefault(discount[i+STREIGHT-1], 0) + 1);
+            if(canJoin(map, discountMap)) answer++;
+        }
+
+
         return answer;
+    }
+    public boolean canJoin(Map<String, Integer> map1, Map<String, Integer> map2) {
+        for(String key : map1.keySet()) {
+            if(!map2.containsKey(key) || map1.get(key) != map2.get(key)) return false;
+        }
+        return true;
     }
 }
